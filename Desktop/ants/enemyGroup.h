05b25cef5e51fb7131ade91f2db77ed3226a95b5
varvara-tsygina <varyatsygina2006@gmail.com>
2025-04-14 20:55:41 +0300

    Add enemyGroup.h

---
+++ Desktop/ants/enemyGroup.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Ant.h"
+#include <vector>
+
+class Enemy {
+public:
+    Enemy();
+    Enemy(int groupSize); // Создаем группу врагов
+    void attackAntHill(std::vector<Ant>& ants); // Атака муравейника (группы муравьев)
+    void printInfo() const;
+
+    bool isAlive() const; // Проверяет, жива ли хоть одна особь в группе
+    int getGroupSize() const { return enemies.size(); }
+
+private:
+    struct EnemyIndividual {  //Внутренняя структура для отдельного врага
+        int health;
+        int strength;
+        bool isAlive;
+
+        EnemyIndividual(int h, int s) : health(h), strength(s), isAlive(true) {}
+    };
+
+    std::vector<EnemyIndividual> enemies; // Вектор отдельных врагов
+    void distributeDamage(int damage); // Метод для распределения урона по группе
+};
